b468f68a7a2b65dcdce6e042508f969cafb282ad
Orkhan Uspatov <orkhanuspatov91@gmail.com>
2025-06-12 20:33:42 +0200

    Dashboard Sort Implementation

--- DataLens/MVVM/ViewModel/HomeViewModel.cs
+++ DataLens/MVVM/ViewModel/HomeViewModel.cs
@@ -2,7 +2,9 @@
 using DataLens.Core;
 using DataLens.MVVM.Model;
 using DataLens.MVVM.Services;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
 using System.Windows.Input;
 
 namespace DataLens.MVVM.ViewModel
@@ -12,11 +14,17 @@ namespace DataLens.MVVM.ViewModel
         public ObservableCollection<Transaction> Transactions { get; set; }
         public ICommand EditExpenseCommand { get; }
         public ICommand DeleteExpenseCommand { get; }
+        public ICommand SortCommand { get; }
+
+        private string _lastSortProperty;
+        private bool _sortDescending;
+
         public HomeViewModel()
         {
             Transactions = new ObservableCollection<Transaction>(TransactionStorage.Load());
             EditExpenseCommand = new RelayCommand(EditExpense);
             DeleteExpenseCommand = new RelayCommand(DeleteExpense);
+            SortCommand = new RelayCommand(Sort);
         }
 
         private void EditExpense(object parameter)
@@ -52,6 +60,54 @@ namespace DataLens.MVVM.ViewModel
                 TransactionStorage.Save(Transactions);
             }
         }
+
+        private void Sort(object parameter)
+        {
+            string property = parameter as string;
+            if (string.IsNullOrEmpty(property)) return;
+
+            if (_lastSortProperty == property)
+            {
+                _sortDescending = !_sortDescending;
+            }
+            else
+            {
+                _sortDescending = false;
+                _lastSortProperty = property;
+            }
+
+            IEnumerable<Transaction> sorted;
+            switch (property)
+            {
+                case "Name":
+                    sorted = _sortDescending
+                        ? Transactions.OrderByDescending(t => t.Name)
+                        : Transactions.OrderBy(t => t.Name);
+                    break;
+                case "Date":
+                    sorted = _sortDescending
+                        ? Transactions.OrderByDescending(t => t.Date)
+                        : Transactions.OrderBy(t => t.Date);
+                    break;
+                case "Category":
+                    sorted = _sortDescending
+                        ? Transactions.OrderByDescending(t => t.Category)
+                        : Transactions.OrderBy(t => t.Category);
+                    break;
+                case "Amount":
+                    sorted = _sortDescending
+                        ? Transactions.OrderByDescending(t => t.Amount)
+                        : Transactions.OrderBy(t => t.Amount);
+                    break;
+                default:
+                    sorted = Transactions;
+                    break;
+            }
+
+            Transactions = new ObservableCollection<Transaction>(sorted);
+            OnPropertyChanged(nameof(Transactions));
+        }
+
 
         public void AddTransaction(Transaction tx)
         {
